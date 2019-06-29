52d9bbea446bbdd08f3d66c8b528ce14311ffdd9
Artem Kulakov <artem.kulakov@gmail.com>
2019-06-29 23:32:12 +0300

    events/new: categories from array

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,6 +15,32 @@ class EventsController < ApplicationController
   # GET /events/new
   def new
     @event = Event.new
+    @categories = [
+      { title: 'Food & Drinks', image: '/images/event-icon1.png' },
+      { title: 'Night Life', image: '/images/event-icon2.png' },
+      { title: 'Outdoors & Adventure', image: '/images/event-icon3.png' },
+      { title: 'Tech', image: '/images/event-icon4.png' },
+      { title: 'Self Improvement', image: '/images/event-icon5.png' },
+      { title: 'Sports', image: '/images/event-icon6.png' },
+      { title: 'Learning', image: '/images/event-icon7.png' },
+      { title: 'Travel', image: '/images/event-icon8.png' },
+      { title: 'Music & Dance', image: '/images/event-icon9.png' },
+      { title: 'Single\'s Only', image: '/images/event-icon10.png' },
+      { title: 'Family', image: '/images/event-icon11.png' },
+      { title: 'Health & Wellness', image: '/images/event-icon12.png' },
+      { title: 'Hobbies & Crafts', image: '/images/event-icon13.png' },
+      { title: 'Fashion & Beauty', image: '/images/event-icon14.png' },
+      { title: 'Film', image: '/images/event-icon15.png' },
+      { title: 'Beliefs', image: '/images/event-icon16.png' },
+      { title: 'LGBTQ', image: '/images/event-icon17.png' },
+      { title: 'Language & Culture', image: '/images/event-icon18.png' },
+      { title: 'Photography', image: '/images/event-icon19.png' },
+      { title: 'Book Club', image: '/images/event-icon20.png' },
+      { title: 'Pets', image: '/images/event-icon21.png' },
+      { title: 'Arts', image: '/images/event-icon22.png' },
+      { title: 'Seminar & Talks', image: '/images/event-icon23.png' },
+      { title: 'Freelance Community', image: '/images/event-icon24.png' },
+    ]
   end
 
   # GET /events/1/edit
@@ -69,6 +95,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :categories_id, :description, :city, :address, :date)
+      params.require(:event).permit(:title, :description, :city, :address, :date)
     end
 end
