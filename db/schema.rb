747658a79c66c3aef9da7f2d3e877e8a1e05d78e
Artem Kulakov <artem.kulakov@gmail.com>
2019-06-30 11:20:34 +0300

    Add image to Event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_29_201359) do
+ActiveRecord::Schema.define(version: 2019_06_30_081859) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_06_29_201359) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "categories"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
