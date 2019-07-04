baaa945ce336947af771d962c905571f1017a20b
Artem Kulakov <artem.kulakov@gmail.com>
2019-07-04 23:16:03 +0300

    events/new: add owner

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -96,6 +96,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :categories, :description, :city, :address, :date)
+      params.require(:event).permit(:title, :categories, :description, :city, :address, :date, :user_id)
     end
 end
