a22fa7053a38b8018f8efe800292e9cc92bc73de
Artem Kulakov <artem.kulakov@gmail.com>
2019-07-05 10:15:39 +0300

    Blog scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_04_193846) do
+ActiveRecord::Schema.define(version: 2019_07_05_071432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id"
+    t.text "body"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string "title"
@@ -49,5 +59,6 @@ ActiveRecord::Schema.define(version: 2019_07_04_193846) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "blogs", "users"
   add_foreign_key "events", "users"
 end
