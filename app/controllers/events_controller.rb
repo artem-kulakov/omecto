c8d74398e2f70d8d0cea404f51ec40481b10589e
Artem Kulakov <artem.kulakov@gmail.com>
2019-07-05 15:50:14 +0300

    events/new: switch to Category model

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,32 +16,7 @@ class EventsController < ApplicationController
   # GET /events/new
   def new
     @event = Event.new
-    @categories = [
-      { title: 'Food & Drinks', image: '/images/event-icon1.png' },
-      { title: 'Night Life', image: '/images/event-icon2.png' },
-      { title: 'Outdoors & Adventure', image: '/images/event-icon3.png' },
-      { title: 'Tech', image: '/images/event-icon4.png' },
-      { title: 'Self Improvement', image: '/images/event-icon5.png' },
-      { title: 'Sports', image: '/images/event-icon6.png' },
-      { title: 'Learning', image: '/images/event-icon7.png' },
-      { title: 'Travel', image: '/images/event-icon8.png' },
-      { title: 'Music & Dance', image: '/images/event-icon9.png' },
-      { title: 'Single\'s Only', image: '/images/event-icon10.png' },
-      { title: 'Family', image: '/images/event-icon11.png' },
-      { title: 'Health & Wellness', image: '/images/event-icon12.png' },
-      { title: 'Hobbies & Crafts', image: '/images/event-icon13.png' },
-      { title: 'Fashion & Beauty', image: '/images/event-icon14.png' },
-      { title: 'Film', image: '/images/event-icon15.png' },
-      { title: 'Beliefs', image: '/images/event-icon16.png' },
-      { title: 'LGBTQ', image: '/images/event-icon17.png' },
-      { title: 'Language & Culture', image: '/images/event-icon18.png' },
-      { title: 'Photography', image: '/images/event-icon19.png' },
-      { title: 'Book Club', image: '/images/event-icon20.png' },
-      { title: 'Pets', image: '/images/event-icon21.png' },
-      { title: 'Arts', image: '/images/event-icon22.png' },
-      { title: 'Seminar & Talks', image: '/images/event-icon23.png' },
-      { title: 'Freelance Community', image: '/images/event-icon24.png' },
-    ]
+    @categories = Category.all
   end
 
   # GET /events/1/edit
