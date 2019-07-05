f6cf7b2f180f1e49badc05937ecb3dc710226494
Artem Kulakov <artem.kulakov@gmail.com>
2019-07-05 23:40:15 +0300

    categories/show: show events for category - 2

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,16 +3,12 @@ class CategoriesController < ApplicationController
 
   def show
     @events = Event.where("categories like ?", "%#{@category.cat_id}%")
+    render 'events/index'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event
       @category = Category.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def event_params
-      # params.require(:category).permit(:title, :categories, :description, :city, :address, :date, :user_id)
     end
 end
