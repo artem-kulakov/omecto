ab021dd239c6a184e4ea61a8dcfdae70646eebcd
Artem Kulakov <artem.kulakov@gmail.com>
2019-07-08 00:11:29 +0300

    events/new & index: image via ActiveStorage

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -71,6 +71,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :categories, :description, :city, :address, :date, :user_id)
+      params.require(:event).permit(:title, :categories, :description, :city, :address, :date, :user_id, :image)
     end
 end
