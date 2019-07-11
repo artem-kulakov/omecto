5099d447dac1258b79765697907515cbd4cf9dc1
Artem Kulakov <artem.kulakov@gmail.com>
2019-07-12 00:02:11 +0300

    Connection model

---
+++ db/migrate/20190711204844_create_connections.rb
@@ -0,0 +1,11 @@
+class CreateConnections < ActiveRecord::Migration[5.2]
+  def change
+    create_table :connections do |t|
+      t.integer :sender_id
+      t.integer :recipient_id
+      t.boolean :accepted, default: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_202240) do
+ActiveRecord::Schema.define(version: 2019_07_11_204844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,14 @@ ActiveRecord::Schema.define(version: 2019_07_08_202240) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "cat_id"
+  end
+
+  create_table "connections", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.boolean "accepted", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "conversations", force: :cascade do |t|
