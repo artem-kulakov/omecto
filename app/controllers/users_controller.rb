ef4187fddb0027293ad95ad82545efd9264c2dd2
Artem Kulakov <artem.kulakov@gmail.com>
2019-07-11 13:12:15 +0300

    users/show - 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show]
+
   def show
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:id)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 	has_many :events
+  has_many :participations
   has_many :events, through: :participations
 	has_many :blogs
 
